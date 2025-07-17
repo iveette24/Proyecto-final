b70aaed6736ccd07a70727363a6f48451517e185
David Romero San Millán <david.romtrad@gmail.com>
2025-07-17 10:37:46 +0200

    TSX borrado de css

--- Prueba-tsx/src/Header.tsx
+++ Prueba-tsx/src/Header.tsx
@@ -1,11 +1,23 @@
 import { Link, NavLink } from "react-router-dom";
-import type { NavLinkProps } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 Object.values(import.meta.glob("./assets/css/*", { eager: true }));
 
 function Header() {
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const onScroll = () => {
+      setScrolled(window.scrollY > 2); // Adjust threshold as needed
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
   return (
     <>
-      <div className="ae-container-fluid ae-container-fluid--full rk-header">
+      <div
+        className={` rk-header${scrolled ? " rk-header--scrolled" : ""}`}
+        style={{ padding: 1 }}
+      >
         <input type="checkbox" id="mobile-menu" className="rk-mobile-menu" />
         <label htmlFor="mobile-menu">
           <svg>
@@ -20,32 +32,64 @@ function Header() {
         </label>
         <div className="ae-container-fluid rk-topbar">
           <h1 className="rk-logo">
-            <Link to="/">KOMUNI</Link>
+            <Link to="/">
+              <img
+                className="logo-komuni"
+                src="src/assets/img/Komuni2.png"
+                alt="Logo de KOMUNI, con una imagen de una chincheta de mapa con una silla de ruedas en azul."
+              />
+            </Link>
           </h1>
           <nav className="rk-navigation">
             <ul className="rk-menu">
               <li className="rk-menu__item">
-                <NavLink to="/"  end className={({ isActive }: { isActive: boolean }) => "rk-menu__link" + (isActive ? " active" : "")}>
+                <NavLink
+                  to="/"
+                  end
+                  className={({ isActive }: { isActive: boolean }) =>
+                    "rk-menu__link" + (isActive ? " active" : "")
+                  }
+                >
                   Home
                 </NavLink>
               </li>
               <li className="rk-menu__item">
-                <NavLink to="/about" className={({ isActive }: { isActive: boolean }) => "rk-menu__link" + (isActive ? " active" : "")}>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }: { isActive: boolean }) =>
+                    "rk-menu__link" + (isActive ? " active" : "")
+                  }
+                >
                   Sobre nosotros
                 </NavLink>
               </li>
               <li className="rk-menu__item">
-                <NavLink to="/mapa" className={({ isActive }: { isActive: boolean }) => "rk-menu__link" + (isActive ? " active" : "")}>
+                <NavLink
+                  to="/mapa"
+                  className={({ isActive }: { isActive: boolean }) =>
+                    "rk-menu__link" + (isActive ? " active" : "")
+                  }
+                >
                   Mapa
                 </NavLink>
               </li>
               <li className="rk-menu__item">
-                <NavLink to="/foro" className={({ isActive }: { isActive: boolean }) => "rk-menu__link" + (isActive ? " active" : "")}>
+                <NavLink
+                  to="/foro"
+                  className={({ isActive }: { isActive: boolean }) =>
+                    "rk-menu__link" + (isActive ? " active" : "")
+                  }
+                >
                   Foro
                 </NavLink>
               </li>
               <li className="rk-menu__item">
-                <NavLink to="/contacto" className={({ isActive }: { isActive: boolean }) => "rk-menu__link" + (isActive ? " active" : "")}>
+                <NavLink
+                  to="/contacto"
+                  className={({ isActive }: { isActive: boolean }) =>
+                    "rk-menu__link" + (isActive ? " active" : "")
+                  }
+                >
                   Contacto
                 </NavLink>
               </li>
